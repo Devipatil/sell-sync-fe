0d018f8db6fdd107a94d1291770cb59eaf1cccb0
Devipatil <devipatil97@gmail.com>
2025-07-14 11:10:17 +0530

    auth-1

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function  Welcome() {
+    return (
+        <>
+        <h3>Welcome page</h3>
+        <button>SIGN UP</button>
+        <br></br><br></br>
+        <button>SIGN IN</button>
+        </>
+    )
+}
+
