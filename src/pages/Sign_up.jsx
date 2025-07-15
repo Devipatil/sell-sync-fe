5ba7fe06cb62d622aac27e44a807871533fdf398
Devipatil <devipatil97@gmail.com>
2025-07-15 22:32:19 +0530

    sign_up

--- src/pages/Sign_up.jsx
+++ src/pages/Sign_up.jsx
@@ -5,36 +5,69 @@ export default function Sign_up() {
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [gender, setGender] = usestate("")
+  const [gender, setGender] = useState("")
   const [dob, setDob] = useState("")
   const [role, setRole] = useState("")
+
+
+   async function handleSubmit(e) {
+    e.preventDefault()
+
+    const data = {
+      username,
+      email,
+      password,
+      gender,
+      dob,
+      role
+    };
+
+    try {
+      const resp = await fetch('http://localhost:8080/signUp', {
+      method : "POST",
+      headers : {
+        "content-Type": "application/json"
+      },
+      body : JSON.stringify(data)
+    });
+
+    const msg = await resp.text()
+    alert(msg)
+    } 
+    catch (error) {
+      console.error('Error:', error)
+      alert('Failed to send data')
+    }
+  }
 
   return (
     <>
     <h4>Sign up below</h4>
- <form>
+ <form onSubmit = {handleSubmit}>
         <label>Username </label>
-        <input type = "text" name = "username"></input>
+        <input type = "text" name = "username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
         <br></br><br></br>
         <label>Email </label>
-        <input type = "email" name = "email"></input>
+        <input type = "email" name = "email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
         <br></br><br></br>
         <label>Password </label>
-        <input type = " password" name = "password"></input>
+        <input type = " password" name = "password" value = {password} onChange={(e) => setPassword(e.target.value)}></input>
         <br></br><br></br>
+
         <label>Gender : </label>
-        Male<input type = "radio" name = " gender" value = "M"></input>
-        Female<input type = "radio" name = " gender" value = "F"></input>
-        Other<input type = "radio" name = " gender" value = "O"></input>
+        Male<input type = "radio" name = " gender" value = "M" checked={gender === "M"} onChange={(e) => setGender(e.target.value)}></input>
+        Female<input type = "radio" name = " gender" value = "F" checked={gender === "F"} onChange={(e) => setGender(e.target.value)}></input>
+        Other<input type = "radio" name = " gender" value = "O" checked={gender === "O"} onChange={(e) => setGender(e.target.value)}></input>
        <br></br><br></br>
+
         <label>DOB </label>
-        <input type = "date" name = "dob"></input>
+        <input type = "date" name = "dob" value = {dob} onChange={(e) => setDob(e.target.value)}></input>
         <br></br><br></br>
         <label>Role : </label>
-        ADMIN<input type = "radio" name = "role" value = "admin"></input>
-        CUSTOMER <input type = "radio" name = "role" value = "customer"></input>
+        ADMIN<input type = "radio" name = "role" value = "admin" checked={role === "admin"} onChange={(e) => setRole(e.target.value)}></input>
+        CUSTOMER <input type = "radio" name = "role" value = "customer" checked={role === "customer"} onChange={(e) => setRole(e.target.value)}></input>
        <br></br><br></br>
-        <input type = "submit" value = "SIGN UP"></input>
+        <button type = "submit">SIGN UP</button>
 </form>
         
     </>
